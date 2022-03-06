ef60c7055a0874426c090b8f808234dc0495cf5c
kenun99 <cswmchen@gmail.com>
2022-03-06 14:48:38 +0800

    add src

---
+++ src/cwm.sh
@@ -0,0 +1,1 @@
+python bin/gen_exploit.py ./tests/data/test1.contract.code 0x1234 0x1000 +1000
---
+++ src/ponzi.sh
@@ -0,0 +1,1 @@
+python bin/gen_exploit.py ~/SADPonzi/ponziTest/data/handover.contract.code 0x1234 0x1000 +1000
